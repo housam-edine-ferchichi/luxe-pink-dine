4625f7ffc527138e6d7b0c1cc27160e89d93415b
housam-edine-ferchichi <64272442+housam-edine-ferchichi@users.noreply.github.com>
2025-04-01 15:48:46 +0000

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+
 import path from "path";
+
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: "/luxe-pink-dine/",
